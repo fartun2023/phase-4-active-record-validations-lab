ea23654aca37dedcfcae6dac803064495710c278
fartun <fartunwario34@gmail.com>
2023-06-24 01:05:55 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :title_must_be_clickbait
+  
+  private
+
+  def title_must_be_clickbait
+    clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+    if title.present? && !clickbait_phrases.any? { |phrase| title.include?(phrase) }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
